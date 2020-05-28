1df46cb2f45c575dcdfa74e48dec4f4cda9b77c8
Miguel Leve <miguel.levezinho@tecnico.ulisboa.pt>
2020-05-28 04:49:23 +0100

    Cleaned minor.l and prepared directory for pratical test

---
+++ P2/teste_pratico/run/expr.c
@@ -0,0 +1,39 @@
+
+#ifdef underscore
+#define power _power
+#define strcompr _strcompr
+#endif
+
+int power(int base, int exp)
+{
+  int i = exp;
+  int res = 1;
+
+  while (i > 0)
+  {
+    res *= base;
+    i--;
+  }
+
+  return res;
+}
+
+int strcompr(const char *a, const char *b)
+{
+  unsigned char c1, c2;
+
+  while (1)
+  {
+    c1 = *a++;
+    c2 = *b++;
+
+    if (c1 != c2)
+      return c1 < c2 ? -1 : 1;
+    
+    if (!c1)
+      break;
+  }
+
+  return 0;
+}
+
